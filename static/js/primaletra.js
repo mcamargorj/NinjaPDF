4cb39b29ec2e2697703dca4d173ec32f7abc57aa
mcamargorj <mcamargorj@gmail.com>
2024-06-14 16:46:52 -0300

    Upload de projeto Ninja_PDF

--- static/js/primaletra.js
+++
@@ -1,3 +0,0 @@
-var usernameElement = document.getElementById('username');
-var username = usernameElement.textContent;
-usernameElement.textContent = username.charAt(0).toUpperCase() + username.slice(1);
